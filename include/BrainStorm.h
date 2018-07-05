9d050b3a267b3444648dcc42915a3bf9c27fe39e
Hakime Seddik <hakime@lowtem.hokudai.ac.jp>
2018-07-05 14:58:42 +0900

    Add a master header file.

---
+++ include/BrainStorm.h
@@ -0,0 +1,22 @@
+//
+//  BrainStorm.h
+//  BrainStorm
+//
+//  Created by Hakime Seddik on 05/07/2018.
+//  Copyright © 2018 Hakime Seddik. All rights reserved.
+//
+
+#ifndef BrainStorm_h
+#define BrainStorm_h
+
+#include "Data.h"
+#include "Memory.h"
+#include "MetalCompute.h"
+#include "NetworkUtils.h"
+#include "NeuralNetwork.h"
+#include "Optimization.h"
+#include "Parsing.h"
+#include "TimeProfile.h"
+#include "Utils.h"
+
+#endif /* BrainStorm_h */
